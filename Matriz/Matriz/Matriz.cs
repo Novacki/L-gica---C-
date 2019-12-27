9690d12d2fc546f3e61a5a79a1fe7202ef3fa0a3
Novacki <nandodetaqua@gmail.com>
2019-12-27 10:28:04 -0300

    Operações com Matriz utilizando Classes

---
+++ Matriz/Matriz/Matriz.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matriz
+{
+    class Matriz
+    {
+        public static int[,] EnterDataMatriz(int row, int col)
+        {
+            int[,] matriz = new int[row, col];
+            for(int i = 0; i < row; i++)
+            {
+                Console.WriteLine("Digite a " + (i + 1) + "° Linha: ");
+                string[] values = Console.ReadLine().Split(" ");
+
+                for(int j = 0; j < col; j++)
+                {
+                    matriz[i, j] = int.Parse(values[j]);   
+                }
+            }
+
+            return matriz;
+        }
+        public static void PrintMatriz(int[,] matriz, int row, int col)
+        {
+            for(int i = 0; i < row; i++)
+            {
+                for(int j = 0; j < col; j++)
+                {
+                    Console.Write(matriz[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        
+        public static void IdentityNumber(int number, int[,] matriz, int row, int col)
+        {
+            for(int i = 0; i < row; i++)
+            {
+                for(int j = 0; j < col; j++)
+                {
+                    if(number == matriz[i, j])
+                    {
+                        Console.WriteLine("Row: " + (i + 1) + " Col: " + (j + 1) + ": " + number);
+
+                        if(j != 0)
+                            Console.WriteLine("Left: " + matriz[i,(j - 1)]);
+                        if(j != (col - 1))
+                            Console.WriteLine("Right: " + matriz[i, (j + 1)]);
+                        if(i != 0)
+                            Console.WriteLine("Top: " + matriz[(i - 1), j]);
+                        if(i != (row - 1))
+                            Console.WriteLine("Botton: " + matriz[(i + 1), j]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Matriz/Matriz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter with the quantity rows: ");
+            int row = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter with the quantity cols: ");
+            int col = int.Parse(Console.ReadLine());
+
+            int[,] matriz = Matriz.EnterDataMatriz(row, col);
+
+            Matriz.PrintMatriz(matriz, row, col);
+
+
+            Console.WriteLine("Enter with number ");
+            int number = int.Parse(Console.ReadLine());
+
+            Matriz.IdentityNumber(number, matriz, row, col);
+
+         
+         
+        }
+
+    }
+}
